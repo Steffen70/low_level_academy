4b798d1aa70c61a3d2f861a93e2bb7f06ccb6c94
Steffen70 <steffen@seventy.mx>
2025-10-03 18:33:35 +0200

    Completed File I/O Exercise.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,54 @@
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void print_the_flag()
 {
-    printf("Hello, World!");
+    FILE* fPtr = fopen("/tmp/flag", "r");
+    char* fileContentBufPtr = nullptr;
+
+    if (fPtr == nullptr)
+    {
+        perror("fopen");
+        return;
+    }
+
+    if (fseek(fPtr, 0, SEEK_END) != 0)
+    {
+        perror("fseek");
+        goto cleanup;
+    }
+
+    const long fileSize = ftell(fPtr);
+    if (fileSize < 0)
+    {
+        perror("ftell");
+        goto cleanup;
+    }
+
+    fileContentBufPtr = malloc(fileSize + 1);
+
+    rewind(fPtr);
+    if (fread(fileContentBufPtr, 1, fileSize, fPtr) != fileSize)
+    {
+        perror("fread");
+        goto cleanup;
+    }
+
+    fileContentBufPtr[fileSize] = '\0';
+
+    printf("%s\n", fileContentBufPtr);
+
+cleanup:
+    fclose(fPtr);
+    if (fileContentBufPtr != nullptr)
+    {
+        free(fileContentBufPtr);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    print_the_flag();
     return 0;
 }
