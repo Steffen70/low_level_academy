4e42bf685e18bae92fa2064c60d4664896ebe241
Steffen70 <steffen@seventy.mx>
2025-10-17 14:44:06 +0200

    Finished Creating a Unix Command Line Utility with Getopt.

--- src/main.c
+++ src/main.c
@@ -1,54 +1,61 @@
-#include <stddef.h>
+#include <getopt.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-void print_the_flag()
+static void print_help(char* argv[])
 {
-    FILE* fPtr = fopen("/tmp/flag", "r");
-    char* fileContentBufPtr = nullptr;
+    printf("Usage:\t%s <database_file> [options]...\n", argv[0]);
+    printf("Options:\n");
 
-    if (fPtr == nullptr)
-    {
-        perror("fopen");
-        return;
-    }
-
-    if (fseek(fPtr, 0, SEEK_END) != 0)
-    {
-        perror("fseek");
-        goto cleanup;
-    }
-
-    const long fileSize = ftell(fPtr);
-    if (fileSize < 0)
-    {
-        perror("ftell");
-        goto cleanup;
-    }
-
-    fileContentBufPtr = malloc(fileSize + 1);
-
-    rewind(fPtr);
-    if (fread(fileContentBufPtr, 1, fileSize, fPtr) != fileSize)
-    {
-        perror("fread");
-        goto cleanup;
-    }
-
-    fileContentBufPtr[fileSize] = '\0';
-
-    printf("%s\n", fileContentBufPtr);
-
-cleanup:
-    fclose(fPtr);
-    if (fileContentBufPtr != nullptr)
-    {
-        free(fileContentBufPtr);
-    }
+    const char* format = "\t%s\t\t%s\n";
+    printf(format, "-h|--help", "show help");
+    printf(format, "-v|--version", "show version");
+    printf(format, "-n|--new", "create new db file");
+    printf(format, "-f|--file", "open existing db file");
 }
 
-int main(int argc, char** argv)
+typedef struct option option;
+
+int main(const int argc, char* argv[])
 {
-    print_the_flag();
+    int longOptIndex = 0;
+
+    static option longOptions[] = {
+        {"help", no_argument, nullptr, 'h'},
+        {"version", no_argument, nullptr, 'v'},
+        {"new", no_argument, nullptr, 'n'},
+        {"file", no_argument, nullptr, 'f'},
+    };
+
+    int opt;
+    if ((opt = getopt_long(argc, argv, "hvnf", longOptions, &longOptIndex)) != -1)
+    {
+        do
+        {
+            switch (opt)
+            {
+            case 'h':
+                print_help(argv);
+                break;
+            case 'v':
+                printf("1.0.0.0\n");
+                break;
+            case 'n':
+                printf("Creating new db file: '%s'...\n", argv[1]);
+                break;
+            case 'f':
+                printf("Using existing db file: '%s'...\n", argv[1]);
+                break;
+            default:
+            case '?':
+                return 1;
+            }
+        }
+        while ((opt = getopt_long(argc, argv, "hvnf", longOptions, &longOptIndex)) != -1);
+    }
+    else
+    {
+        printf("Using existing db file: '%s'...\n", argv[optind]);
+    }
+
     return 0;
 }
